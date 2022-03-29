dcb345f6cbd2f1c077b9744a02c200e211aecee6
nicholasl98 <63031128+nicholasl98@users.noreply.github.com>
2022-03-29 15:29:11 -0700

    updated dashboard files

--- dashboard-ui/src/components/AppStats.js
+++ dashboard-ui/src/components/AppStats.js
@@ -8,7 +8,7 @@ export default function AppStats() {
 
 	const getStats = () => {
 	
-        fetch(`http://<Cloud DNS>:8100/stats`)
+        fetch(`http://kafka-nicholas.eastus2.cloudapp.azure.com:8100/stats`)
             .then(res => res.json())
             .then((result)=>{
 				console.log("Received Stats")
@@ -35,22 +35,22 @@ export default function AppStats() {
                 <table className={"StatsTable"}>
 					<tbody>
 						<tr>
-							<th>Blood Pressure</th>
-							<th>Heart Rate</th>
+							<th>Member Check in</th>
+							<th>Gym Equipment In Use</th>
 						</tr>
 						<tr>
-							<td># BP: {stats['num_bp_readings']}</td>
-							<td># HR: {stats['num_hr_readings']}</td>
+							<td># BP: {stats['num_membercheckin_readings']}</td>
+							<td># HR: {stats['num_gymequipmentuse_readings']}</td>
 						</tr>
 						<tr>
-							<td colspan="2">Max BP Systolic: {stats['max_bp_sys_reading']}</td>
+							<td colspan="2">Oldest Member Age: {stats['max_member_age_reading']}</td>
 						</tr>
 						<tr>
-							<td colspan="2">Max BR Diastolic: {stats['max_bp_dia_reading']}</td>
+							<td colspan="2">Most used machine: {stats['max_machine_name_reading']}</td>
 						</tr>
-						<tr>
+						{/* <tr>
 							<td colspan="2">Max HR: {stats['max_bp_sys_reading']}</td>
-						</tr>
+						</tr> */}
 					</tbody>
                 </table>
                 <h3>Last Updated: {stats['last_updated']}</h3>
