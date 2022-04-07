db3d30c2aab68d2792462ad50202ef8407cf8927
nicholasl98 <63031128+nicholasl98@users.noreply.github.com>
2022-04-07 13:07:49 -0700

    Add files via upload

--- Health/app.py
+++ Health/app.py
@@ -1,4 +1,3 @@
-
 import requests
 import connexion
 import yaml
--- Health/health.py
+++ Health/health.py
@@ -1,33 +1,36 @@
-from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime 
-from base import Base 
-class Health(Base): 
-    """ Processing health """ 
- 
-    __tablename__ = "health" 
- 
-    id = Column(Integer, primary_key=True) 
-    receiver = Column(String, nullable=False) 
+from sqlalchemy import Column, Integer, String, DateTime
+from base import Base
+
+
+class Health(Base):
+    """ Processing Statistics """
+
+    __tablename__ = "health"
+
+    id = Column(Integer, primary_key=True)
+    receiver = Column(String, nullable=False)
     storage = Column(String, nullable=False)
-    processing = Column(String, nullable=False) 
+    processing = Column(String, nullable=False)
     audit = Column(String, nullable=False)
-    last_updated = Column(String, nullable=False)
- 
-    def __init__(self, receiver, storage, processing, audit ,last_updated): 
-        """ Initializes a processing health objet """ 
+    last_update = Column(DateTime, nullable=False)
+
+
+    def __init__(self, receiver, storage, processing, audit, last_update):
+        """ Initializes a processing statistics object """
         self.receiver = receiver
         self.storage = storage
         self.processing = processing
         self.audit = audit
-        self.last_updated = last_updated 
- 
-    def to_dict(self): 
-        """ Dictionary Representation of health """ 
-        dict = {} 
+        self.last_update = last_update
+
+
+    def to_dict(self):
+        """ Dictionary Representation of a statistics """
+        dict = {}
         dict['receiver'] = self.receiver
         dict['storage'] = self.storage
         dict['processing'] = self.processing
         dict['audit'] = self.audit
-        dict['last_updated'] = self.last_updated
- 
+        dict['last_update'] = self.last_update.strftime("%Y-%m-%dT%H:%M:%SZ")
+
         return dict
